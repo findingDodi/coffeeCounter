4d6b4f7868d938c26e339e5ee8558c6f1493b0a1
findingDodi <dorina.diem@gmail.com>
2023-12-10 14:38:00 +0100

    refactoring

--- coffeeCounter.py
+++ coffeeCounter.py
@@ -3,7 +3,7 @@ import json
 import os.path
 
 
-COFFEE_BASE_FILE = "coffee_base.json"
+COFFEE_BASE_FILE = 'coffee_base.json'
 
 
 def track_coffee_for_today():
@@ -15,7 +15,7 @@ def track_coffee_for_today():
 
 def add_coffee_for_today():
     new_value = int(input('Please enter your amount of coffee you want to add for today: '))
-    with open(COFFEE_BASE_FILE, "r") as read_file:
+    with open(COFFEE_BASE_FILE, 'r') as read_file:
         data = json.load(read_file)
 
     key = str(date.today())
@@ -36,7 +36,7 @@ def track_coffee_for_another_day():
 
 
 def print_coffee_consumption():
-    with open(COFFEE_BASE_FILE, "r") as read_file:
+    with open(COFFEE_BASE_FILE, 'r') as read_file:
         data = json.load(read_file)
 
     sorted_data = dict(sorted(data.items()))
@@ -44,40 +44,40 @@ def print_coffee_consumption():
 
     for data_entry in sorted_data.items():
         print(
-            "{}:{}".format(
-                str(data_entry[0]).ljust(15, " "),
-                str(data_entry[1]).rjust(3, " ")
+            '{}:{}'.format(
+                str(data_entry[0]).ljust(15, ' '),
+                str(data_entry[1]).rjust(3, ' ')
             )
         )
         summed_up += data_entry[1]
 
     print(
-        "{}:{}".format(
-            "Total Coffees".ljust(15, " "),
-            str(summed_up).rjust(3, " ")
+        '{}:{}'.format(
+            'Total Coffees'.ljust(15, ' '),
+            str(summed_up).rjust(3, ' ')
         )
     )
 
 
 def save_data(user_date, user_amount):
     if not os.path.isfile(COFFEE_BASE_FILE):
-        with open(COFFEE_BASE_FILE, "w") as write_file:
+        with open(COFFEE_BASE_FILE, 'w') as write_file:
             json.dump({}, write_file)
 
-    with open(COFFEE_BASE_FILE, "r") as read_file:
+    with open(COFFEE_BASE_FILE, 'r') as read_file:
         data = json.load(read_file)
 
     data[user_date] = user_amount
 
-    with open(COFFEE_BASE_FILE, "w") as write_file:
+    with open(COFFEE_BASE_FILE, 'w') as write_file:
         json.dump(data, write_file)
 
 
-if __name__ == "__main__":
-    print("[1] Track Coffee for today")
-    print("[2] Add Coffee for today")
-    print("[3] Track Coffee for another day")
-    print("[4] Show Coffee consumption")
+if __name__ == '__main__':
+    print('[1] Track Coffee for today')
+    print('[2] Add Coffee for today')
+    print('[3] Track Coffee for another day')
+    print('[4] Show Coffee consumption')
     user_input_number = int(input('Please choose a number: '))
 
     if user_input_number == 1:
